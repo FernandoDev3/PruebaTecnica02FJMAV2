136fe1c7fe27c87faefeee0da51e10cc612bde68
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-02-24 22:13:09 -0600

    Clase Celular: Creada

---
+++ PruebaTecnica02FJMAV2/Models/Celulare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruebaTecnica02FJMAV2.Models
+{
+    public partial class Celulare
+    {
+        public int CelularId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal Precio { get; set; }
+        public string? Descripcion { get; set; }
+        public byte[]? Imagen { get; set; }
+        public int MarcaId { get; set; }
+
+        public virtual Marca Marca { get; set; } = null!;
+    }
+}
