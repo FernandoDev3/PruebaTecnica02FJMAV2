e23053114fb6005239492447e58c5f54d8e76a2b
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-02-24 22:12:41 -0600

    Clase Marca: Creada

---
+++ PruebaTecnica02FJMAV2/Models/Marca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruebaTecnica02FJMAV2.Models
+{
+    public partial class Marca
+    {
+        public Marca()
+        {
+            Celulares = new HashSet<Celulare>();
+        }
+
+        public int MarcaId { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public virtual ICollection<Celulare> Celulares { get; set; }
+    }
+}
